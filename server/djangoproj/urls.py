9382a67711fd36a291c66f3e6c1c739711c82e9f
Gayatri Kalne <gayatrikalne10@gmail.com>
2026-01-11 16:37:10 -0500

    Fix E251 spacing in Python and missing semicolon in JS

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,12 +19,12 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
+urlpatterns=[
     path('admin/', admin.site.urls),
-    path('register/', TemplateView.as_view(template_name = "index.html")),
-    path('login/', TemplateView.as_view(template_name = "index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
     path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name = "Home.html")),
-    path('about/', TemplateView.as_view(template_name = "About.html")),
-    path('contact/', TemplateView.as_view(template_name ="Contact.html")),
+    path('', TemplateView.as_view(template_name="Home.html")),
+    path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
